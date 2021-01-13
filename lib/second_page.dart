bab7f397678236d0c9f26a1a6d4136b2da17be38
TheSnowFall <erfanmahmud642@gmail.com>
2021-01-13 18:15:22 +0600

    this is a test commit with 2 blank class

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,22 +8,21 @@ class SecondPage extends StatelessWidget {
         title: Text("Add New"),
       ),
       body: MyCustomForm(),
-
     );
   }
 }
 
 // Create a Form widget.
-  class MyCustomForm extends StatefulWidget {
+class MyCustomForm extends StatefulWidget {
   @override
   MyCustomFormState createState() {
-  return MyCustomFormState();
+    return MyCustomFormState();
   }
-  }
+}
 
 // Create a corresponding State class.
 // This class holds data related to the form.
-  class MyCustomFormState extends State<MyCustomForm> {
+class MyCustomFormState extends State<MyCustomForm> {
   // Create a global key that uniquely identifies the Form widget
   // and allows validation of the form.
   //
@@ -34,50 +32,72 @@ class SecondPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  // Build a Form widget using the _formKey created above.
-  return Form(
-  key: _formKey,
-  child:Padding(
-    padding:const EdgeInsets.symmetric(horizontal: 16.0),
-  child: Column(
-  crossAxisAlignment: CrossAxisAlignment.start,
-  children: <Widget>[
-  TextFormField(
-  validator: (value) {
-  if (value.isEmpty) {
-  return 'Please enter some text';
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height:20,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "Enter Title",
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius:  BorderRadius.circular(8.0),
+                  borderSide:  BorderSide(),
+                ),
+                //fillColor: Colors.green
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter Title';
+                }
+                return null;
+              },
+            ),
+            SizedBox(
+              height:20,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "Enter Description",
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius:  BorderRadius.circular(8.0),
+                  borderSide:  BorderSide(),
+                ),
+                //fillColor: Colors.green
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter Description';
+                }
+                return null;
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Validate returns true if the form is valid, or false
+                  // otherwise.
+                  if (_formKey.currentState.validate()) {
+                    // If the form is valid, display a Snackbar.
+                    Scaffold.of(context).showSnackBar(
+                        SnackBar(content: Text('Processing Data')));
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-  return null;
-  },
-  ),
-
-    TextFormField(
-      validator: (value) {
-        if (value.isEmpty) {
-          return 'Please enter some text';
-        }
-        return null;
-      },
-    ),
-
-  Padding(
-  padding: const EdgeInsets.symmetric(vertical: 16.0),
-  child: ElevatedButton(
-  onPressed: () {
-  // Validate returns true if the form is valid, or false
-  // otherwise.
-  if (_formKey.currentState.validate()) {
-  // If the form is valid, display a Snackbar.
-  Scaffold.of(context)
-      .showSnackBar(SnackBar(content: Text('Processing Data')));
-  }
-  },
-  child: Text('Submit'),
-  ),
-  ),
-  ],
-  ),
-  ),
-  );
-  }
-  }
+}
